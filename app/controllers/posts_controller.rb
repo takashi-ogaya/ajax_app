9fafcea039ce87b83b5875a3fede606a702bd3af
takashi-ogaya <takashi.ogaya@gmail.com>
2022-03-07 11:22:43 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,13 +3,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(id: "DESC")
   end
-
-end
-
-  # コメントアウトする
-  # def new
-  # end
-
+ 
+   # コメントアウトする
+   # def new
+   # end
+ 
   def create
     Post.create(content: params[:content])
     redirect_to action: :index  # 追記する
